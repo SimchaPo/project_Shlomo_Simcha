956fc2983bcc5ecf8782ba3652c98c3c43895644
MeerzonShlomo <24510568+MeerzonShlomo@users.noreply.github.com>
2019-03-17 15:12:09 +0200

    tikun,arg is not Vector, itspoint3d

--- tar1_primitives/src/primitives/Vector.java
+++ tar1_primitives/src/primitives/Vector.java
@@ -1,29 +1,40 @@
 package primitives;
+
 import javax.tools.Diagnostic;
 
 import primitives.Point3D;
 
-
 public class Vector {
 	Point3D vectorPoint;
 	Vector vectorUnit;
-	Point3D nullCoordinate= new Point3D();
-	public Vector(Point3D pnt) {
+	static Point3D nullCoordinate = new Point3D();
+
+	public Vector(Point3D pnt,Point3D beginPnt) {
 		vectorPoint=pnt;
 		if (pnt.distance(nullCoordinate)!=1) {
-			
+			vectorUnit=this;
 		}
+		else vectorUnit.vectorUnitDet(beginPnt)
 	}
-	public Vector(Point3D otherPoint) {
-		// TODO Auto-generated constructor stub
+	public Vector(Point3D pnt) {
+		this(pnt,nullCoordinate);
+		if (pnt.distance(nullCoordinate)!=1) {
+			vectorUnit=this;
+		}
+		else vectorUnit.vectorUnitDet()
 	}
+
 	public Vector vectorUnitDet() {
-		Double distnce=this.vectorPoint.distance(nullCoordinate);
-		 return vectorUnit=new Vector((new Point3D(this.vectorPoint.x._coord/distnce,
-				this.vectorPoint.y._coord/distnce,
-				this.vectorPoint.z._coord/distnce)));
-		
+		Double distnce = this.vectorPoint.distance(nullCoordinate);
+		return vectorUnit = new Vector((new Point3D(this.vectorPoint.x._coord / distnce,
+				this.vectorPoint.y._coord / distnce, this.vectorPoint.z._coord / distnce)));
+
 	}
-	public Vector vectorUnitDet(Vector otherVec) {}
+
+	public Vector vectorUnitDet(Point3D otherVecPnt) {
+		Double distnce = this.vectorPoint.distance(otherVecPnt);
+		return vectorUnit = new Vector((new Point3D(this.vectorPoint.x._coord / distnce,
+				this.vectorPoint.y._coord / distnce, this.vectorPoint.z._coord / distnce)));
+	}
 
 }
