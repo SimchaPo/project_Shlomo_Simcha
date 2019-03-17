f248a390f7ef03a943aa1a2a1c6fb42920ca6c71
MeerzonShlomo <24510568+MeerzonShlomo@users.noreply.github.com>
2019-03-17 14:27:28 +0200

    vector

--- tar1_primitives/src/primitives/Vector.java
+++ tar1_primitives/src/primitives/Vector.java
@@ -1,4 +1,6 @@
 package primitives;
+import javax.tools.Diagnostic;
+
 import primitives.Point3D;
 
 
@@ -16,10 +18,11 @@ public class Vector {
 		// TODO Auto-generated constructor stub
 	}
 	public Vector vectorUnitDet() {
-		Double distnce=this.
-		vectorUnit= Point3D(this.vectorPoint.x._coord/vectorPoint.distance(nullCoordinate),
-				this.vectorUnit=vectorPoint.y._coord/vectorPoint.distance(nullCoordinate),
-				this.vectorUn it=vectorPoint.z._coord/vectorPoint.distance(nullCoordinate))
+		Double distnce=this.vectorPoint.distance(nullCoordinate);
+		 return vectorUnit=new Vector((new Point3D(this.vectorPoint.x._coord/distnce,
+				this.vectorPoint.y._coord/distnce,
+				this.vectorPoint.z._coord/distnce)));
+		
 	}
 	public Vector vectorUnitDet(Vector otherVec) {}
 
