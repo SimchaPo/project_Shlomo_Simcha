c78aabfd2cedc9ad4c3804380ce281d755515968
SimchaPo <45201254+SimchaPo@users.noreply.github.com>
2019-03-18 15:08:52 +0200

    a

--- tar1_primitives/src/primitives/test.java
+++ tar1_primitives/src/primitives/test.java
@@ -2,12 +2,21 @@ package primitives;
 
 import primitives.Vector;
 
-import java.lang.Math
+import java.lang.Math;
 
 public class test {
-	public boolean isOrtogonal(Vector vec1, Vector vec2) {
-		double angle = Math.acos(vec1.vectorsDotProduct(vec2)/
+	public double getAngle(Vector vec1, Vector vec2) {
+		return Math.acos(vec1.vectorsDotProduct(vec2)/
 				(vec1.vectorPoint.distance(Vector.nullCoordinate)*vec2.vectorPoint.distance(Vector.nullCoordinate)));
-		
 	}
+	public boolean isOrtogonal(Vector vec1, Vector vec2) {
+		return getAngle(vec1, vec2) == 90;
+	}
+	public boolean isSharpAngle(Vector vec1, Vector vec2) {
+		return getAngle(vec1, vec2) < 90;
+	}
+	public boolean isObtuseAngle(Vector vec1, Vector vec2) {
+		return getAngle(vec1, vec2) > 90;
+	}
+	
 }
