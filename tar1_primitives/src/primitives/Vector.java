a1791aba079ad8e2bfc9c198d9bf9cc8a351d7b4
SimchaPo <45201254+SimchaPo@users.noreply.github.com>
2019-03-18 13:06:52 +0200

    ray

--- tar1_primitives/src/primitives/Vector.java
+++ tar1_primitives/src/primitives/Vector.java
@@ -7,27 +7,20 @@ import primitives.Point3D;
 public class Vector {
 	Point3D vectorPoint;
 	Vector vectorUnit;
-	Point3D nullCoordinate= new Point3D();
+	static Point3D nullCoordinate= new Point3D();
 	public Vector(Point3D pnt) {
-		vectorPoint=pnt;
+		vectorPoint = pnt;
 		if (pnt.distance(nullCoordinate)!=1) {
 			
 		}
 	}
-	public Vector(Point3D otherPoint) {
-		// TODO Auto-generated constructor stub
+	public Vector(Point3D pnt1, Point3D pnt2) {
+		vectorPoint = pnt1.substruct(pnt2).vectorPoint;
 	}
-	public Vector vectorUnitDet() {
+	public Vector vectorUnitDet() {		
 		Double distnace = this.vectorPoint.distance(nullCoordinate);
 		 return new Vector(new Point3D(this.vectorPoint.x._coord/distnace,
 				this.vectorPoint.y._coord/distnace,
-				this.vectorPoint.z._coord/distnace));
+				this.vectorPoint.z._coord/distnace));		 
 	}
-	public Vector vectorUnitDet(Vector otherVec) {
-		Double distance = this.vectorPoint.distance(otherVec.vectorPoint);
-		return new Vector(new Point3D(this.vectorPoint.x._coord/distance,
-				vectorPoint.y._coord/distance,
-				vectorPoint.z._coord/distance));
-	}
-
 }
