6684cd50b9e78c76ecc5a222fae188721e4fb315
SimchaPo <45201254+SimchaPo@users.noreply.github.com>
2019-05-03 09:03:40 +0300

    c

--- tar1_primitives/src/unittests/SphereTest.java
+++ tar1_primitives/src/unittests/SphereTest.java
@@ -1,8 +1,11 @@
 package unittests;
 
+import java.util.List;
+
 import geometries.Sphere;
 import junit.framework.TestCase;
 import primitives.Point3D;
+import primitives.Ray;
 import primitives.Vector;
 
 public class SphereTest extends TestCase {
@@ -17,6 +20,12 @@ public class SphereTest extends TestCase {
 	}
 
 	public void testFindIntersections() {
-		fail("do somthing");
+		Sphere sp = new Sphere(new Point3D(0, 0, 0), 3);
+		System.out.println("abc");
+		List<Point3D> result = sp.findIntersections(new Ray(new Point3D(0, 0,0),new Vector(0, 0, 1)));
+		System.out.println("abc" + result);
+		//Vector exp = new Vector(0, 1, 0);
+		fail("faild");
+		//assertEquals("problem with normal", exp, result);
 	}
 }
