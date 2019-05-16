50671e471f428c93b9625022c924cea3d5bc5adb
SimchaPo <45201254+SimchaPo@users.noreply.github.com>
2019-05-16 12:56:58 +0300

    commit

--- tar1_primitives/src/unittests/TestSax.java
+++ tar1_primitives/src/unittests/TestSax.java
@@ -3,14 +3,16 @@ package unittests;
 import org.xml.sax.SAXException;
 
 import junit.framework.TestCase;
+import parser.SceneDescriptor;
 import parser.SceneXmlParser;
 
 public class TestSax extends TestCase {
 	SceneXmlParser a = new SceneXmlParser();
+	SceneDescriptor b = new SceneDescriptor();
 
 	public void testHandlerTest() throws SAXException {
-		String tString = a.getParserDescriptor().get_sceneAttributes().toString();
-	 System.out.println(tString);
-		assertEquals(true, true);
+		String tString1 = a.getParserDescriptor().get_sceneAttributes().toString();
+		String tString2 = b.get_sceneAttributes().toString();
+		assertEquals(tString1, tString2);
 	}
 }
