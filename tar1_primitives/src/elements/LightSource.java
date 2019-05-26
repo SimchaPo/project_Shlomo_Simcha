ad3d804c56d20aa676e47ad3ccc0a010d7abdea7
SimchaPo <45201254+SimchaPo@users.noreply.github.com>
2019-05-26 14:55:09 +0300

    com

--- tar1_primitives/src/elements/LightSource.java
+++ tar1_primitives/src/elements/LightSource.java
@@ -1,5 +1,11 @@
 package elements;
 
-public interface LightSource {
+import primitives.Color;
+import primitives.Point3D;
+import primitives.Vector;
 
+public interface LightSource {
+	public Color getIntensity(Point3D pnt);
+	public Vector getL(Point3D pnt);
+	public Vector geDL(Point3D pnt);
 }
